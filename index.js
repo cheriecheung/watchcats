6432f10a595de1ee319beaab1511be33c1b43f15
Cherie Cheung <cheriesofteng@gmail.com>
2020-06-25 16:25:37 +0800

    [Auth] Integrate google sign in

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
-dotenv.config();
-mongoose.connect(process.env.DB_CONNECT, () =>
-  console.log('connected to db yay')
+require('dotenv').config();
+require('./config/passport-setup');
+
+mongoose.connect(process.env.DB_CONNECT, () => {
+  console.log('connected to db yay');
+});
+
+// encrypt cookie
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [process.env.SESSION_COOKIE_KEY],
+  })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Route middlewares
-app.use('/api/user', authRoute);
-app.use('/api/posts', postRoute);
-// app.all('/', (req, res) => {
-//   return res.status(200).json({ body: req.body, query: req.query });
-// });
+app.use('/user', authRoute);
+app.use('/posts', postRoute);
 
 app.listen(3000, () => console.log('Server up and running'));
