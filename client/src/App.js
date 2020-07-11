adce0e409176c99d4f7193e1c15ff08c916a57d3
Cherie Cheung <cheriesofteng@gmail.com>
2020-07-12 00:07:51 +0800

    Create private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
-import "./App.css";
+import './App.css';
 
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import Header from "./components/Layout/Header";
-import Layout from "./components/Layout";
-import Home from "./containers/Home";
-import FindSitter from "./containers/FindSitter";
-import About from "./containers/About";
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import Header from './components/Layout/Header';
+import Layout from './components/Layout';
+import Home from './containers/Home';
+import About from './containers/About';
+import Account from './containers/Account';
+import FindSitter from './containers/FindSitter';
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        localStorage.getItem('user') ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+        )
+      }
+    />
+  );
+}
 
 function App() {
   const { i18n } = useTranslation();
 
   useEffect(() => {
-    i18n.changeLanguage(localStorage.getItem("lang") || "en");
+    i18n.changeLanguage(localStorage.getItem('lang') || 'en');
   }, []);
 
   return (
     <div className="App">
-      <BrowserRouter basename={"/"}>
+      <BrowserRouter basename={'/'}>
         <Layout>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/find" component={FindSitter} />
             <Route path="/about" component={About} />
             {/* <Route path="/login" component={Login} /> */}
+            <PrivateRoute path="/account" component={Account} />
           </Switch>
         </Layout>
       </BrowserRouter>
