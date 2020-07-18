875c1df3c7b6872ef4b380973473c47887141bd4
Cherie Cheung <cheriesofteng@gmail.com>
2020-07-19 00:58:09 +0800

    Integrate SMS api

---
+++ server/helpers/sms.js
@@ -0,0 +1,28 @@
+var AWS = require('aws-sdk');
+
+const sendSMS = (phone, code) => {
+  const params = {
+    Message: `Your Watch Cat verification code is ${code}`,
+    PhoneNumber: '+' + phone,
+    MessageAttributes: {
+      'AWS.SNS.SMS.SenderID': {
+        DataType: 'String',
+        StringValue: 'Watch Cat Mobile Authentication',
+      },
+    },
+  };
+
+  const publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31' })
+    .publish(params)
+    .promise();
+
+  publishTextPromise
+    .then((data) => {
+      return res.end(JSON.stringify({ MessageID: data.MessageId }));
+    })
+    .catch((err) => {
+      return res.end(JSON.stringify({ Error: err }));
+    });
+};
+
+module.exports = { sendSMS };
