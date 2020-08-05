d2f8fe7b0ac9c88a9e4e372f515fcf6182f09661
Cherie Cheung <cheriesofteng@gmail.com>
2020-08-05 20:34:37 +0800

    Create member public profile page

--- client/src/containers/FindSitter/index.js
+++ client/src/containers/FindSitter/index.js
@@ -1,21 +1,23 @@
-import React from "react";
-import { useTranslation } from "react-i18next";
-import Map from "./Map";
-import Search from "./Search";
-import ResultDisplay from "./ResultDisplay";
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Map from './Map';
+import Search from './Search';
+import ResultDisplay from './ResultDisplay';
+import Profile from './Profile';
 
 function FindSitter() {
   const { t, i18n } = useTranslation();
 
   return (
-    <div>
-      <Search />
+    <>
+      <Profile />
+      {/* <Search />
       <div style={{ display: "flex" }}>
-        <ResultDisplay />
-        {/* <Map /> */}
-        <div style={{ width: "100%", background: "#E0FFFF" }}></div>
-      </div>
-    </div>
+        <ResultDisplay /> */}
+      {/* <Map /> */}
+      {/* <div style={{ width: "100%", background: "#E0FFFF" }}></div>
+      </div>  */}
+    </>
   );
 }
 
