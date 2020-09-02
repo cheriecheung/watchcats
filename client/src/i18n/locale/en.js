4ef950873392803e520903207af8d3a9279628af
Cherie Cheung <cheriesofteng@gmail.com>
2020-09-03 00:35:33 +0800

    [i18n] Update English / Dutch translation

--- client/src/i18n/locale/en.js
+++ client/src/i18n/locale/en.js
@@ -1,18 +1,19 @@
 export default {
   header: {
     find_sitter: 'Find a cat sitter',
-    // find_cat: 'Find a cat to sit',
     about: 'About',
     bookings: 'Bookings',
     account: 'Account',
     login: 'Login',
     logout: 'Logout',
   },
+
   home: {
     i_am: 'I am a...',
     owner_finds_sitter: 'Cat owner looking for a cat sitter',
     sitter_finds_owner: 'Cat sitter looking for homes to sit.',
   },
+
   form: {
     no_empty: "Field can't be empty",
     only_alphabet: 'Enter alphabets only',
@@ -21,33 +22,147 @@ export default {
     password: 'Password',
     first_name: 'First name',
     last_name: 'Last name',
+    save: 'Save',
+    reset: 'Reset',
   },
+
   login: {
     login: 'Login',
     response: 'Login failed; Invalid user ID or password.',
   },
+
   register: {
     register: 'Register',
-    response:
-      'A link to activate your account has been emailed to the address provided.',
+    response: 'A link to activate your account has been emailed to the address provided.',
     password: 'Password should be 8 to 12 characters',
   },
+
   forgotPassword: {
     response:
       'If that email address is in our database, we will send you an email to reset your password.',
   },
-  general_info: {
-    mobile: 'Mobile number',
-    address1: 'Address 1',
-    address2: 'Address 2',
-    proof_address_label: 'Proof of address',
-    proof_address1:
-      'Before we can make your profile live we need proof of your address. We accept photo/scan of a physical letter or PDF copies of your digital statements within the past year.',
-    proof_address2: 'Read more on the documents we accept',
+
+  account: {
+    general_info: 'General info',
+    cat_sitter_profile: 'Cat sitter profile',
+    cat_owner_profile: 'Cat owner profile',
+    settings: 'Settings',
   },
-  cat_sitter_profile: {
-    about_me_label: 'About me',
-    about_me:
+
+  generalInfo: {
+    profile_picture: 'Profile picture',
+
+    personal_info: 'Personal info',
+    first_name: 'First name',
+    last_name: 'Last name',
+    phone: 'Phone',
+    email: 'Email',
+    address: 'Address',
+    postcode: 'Postcode',
+
+    social_media: 'Social media',
+    facebook: 'Facebook profile',
+    instagram: 'Instagram profile',
+    other: 'Other profile',
+
+    proof_address: 'Proof of address',
+    proof_address_description1:
+      'Before we can make your profile live we need proof of your address. We accept photo/scan of a physical letter or PDF copies of your digital statements within the past year.',
+    proof_address_description2: 'Read more on the documents we accept',
+  },
+
+  catSitterProfile: {
+    view_profile: 'View my public cat sitter profile',
+
+    about_me: 'About me',
+    about_me_description:
       'Tell cat owners about yourself. Start with a little description of yourself - What do you do for a living? Why do you want to be a cat sitter?',
+
+    experience_and_serivce: 'Experience and service',
+    experience_description:
+      'Please select relevant experience / skills you possess. For every item selected, please briefly explain the details in the text box below.',
+    has_cat: 'Owns / owned a cat',
+    volunteer: 'Has done volunteer work',
+    administer_medication: 'Able to administer medication',
+    do_injections: 'Able to do injections',
+    certificate: 'Has animal care certification',
+    grooming_skills: 'Has pet grooming skills',
+
+    pricing: 'Pricing',
+    one_day: 'One day visit',
+    per_hour: 'per hour',
+    overnight: 'Overnight visit',
+    per_night: 'per night',
+
+    availability: 'Availability',
+    availability_description:
+      'Select the dates that you are not available, so that cat owners can send you requests based on your availability.',
+    available: 'Available',
+    unavailable: 'Unavailable',
+
+    emergency_contact: 'Emergency contact',
+    emergency_description: 'In case of an emergency, cat owners can contact the following:',
+    contact_full_name: 'Contact full name',
+    contact_number: 'Contact number',
+  },
+
+  catOwnerProfile: {
+    view_profile: 'View my public cat owner profile',
+
+    about_me: 'About me',
+    about_me_description: '',
+
+    appointment: 'Cat sitting appointment',
+    one_day: 'One day visit',
+    date: 'Date',
+    start_time: 'Start time',
+    end_time: 'End time',
+    overnight: 'Overnight visit',
+    start_date: 'Start date',
+    end_date: 'End date',
+    add_period: 'Add another period',
+    remove: 'Remove',
+
+    about_cat: 'About my cat',
+    name: 'Name',
+    age: 'Age',
+    gender: 'Gender',
+    male: 'Male',
+    female: 'Female',
+    medical_needs: 'Medical needs',
+    injection: 'Injection',
+    pill: 'Pill',
+    vaccinated: 'Vaccinated',
+    insured: 'Insured',
+    yes: 'Yes',
+    no: 'No',
+    breed: 'Breed',
+    favourite_treat: 'Favourite treat',
+    pictures: 'Pictures of your cat',
+    add_cat: 'Add another cat',
+    remove_cat: 'Remove cat',
+
+    cat_description: 'Description of my cat(s)',
+    cat_description_text: '',
+  },
+
+  bookings: {
+    sitting_jobs: 'Sitting jobs for me',
+    sitting_service: 'Sitting service for my cat',
+
+    requested: 'Requested',
+    confirmed: 'Confirmed',
+    completed: 'Completed',
+    reviews: 'Reviews',
+
+    view_profile: 'View profile',
+    view_conversation: 'View conversation',
+
+    area: 'Area',
+    sitter_needed: 'Sitter needed',
+
+    schedule_meetup: 'Schedule a meet-up first',
+    reject: 'Reject',
+    accept: 'Accept',
   },
 };
--- client/src/i18n/locale/nl.js
+++ client/src/i18n/locale/nl.js
@@ -1,18 +1,19 @@
 export default {
   header: {
     find_sitter: 'Oppas zoeken',
-    // find_cat: 'Kat zoeken',
     about: 'Over',
     bookings: 'Bookings',
     account: 'Account',
     login: 'Inloggen',
     logout: 'Uitloggen',
   },
+
   home: {
     i_am: 'Ik ben een...',
     owner_finds_sitter: 'Kat eigenaar op zoek naar een oppas',
     sitter_finds_owner: 'Oppas op zoek naar een kat',
   },
+
   form: {
     no_empty: 'Kan niet leeg blijven',
     only_alphabet: 'Alleen alfabetische tekens',
@@ -21,19 +22,147 @@ export default {
     password: 'Wachtwoord',
     first_name: 'Voornaam',
     last_name: 'Achternaam',
+    save: 'Save',
+    reset: 'Resetten',
   },
+
   login: {
     login: 'Inloggen',
     response: 'Login failed; Invalid user ID or password.',
   },
+
   register: {
     register: 'Registreren',
-    response:
-      'A link to activate your account has been emailed to the address provided.',
+    response: 'A link to activate your account has been emailed to the address provided.',
     password: 'Password should be 8 to 12 characters',
   },
+
   forgotPassword: {
     response:
       'If that email address is in our database, we will send you an email to reset your password.',
+  },
+
+  account: {
+    general_info: 'Algemene informatie',
+    cat_sitter_profile: 'Kattenoppasprofiel',
+    cat_owner_profile: 'Katteneigenaarsprofiel',
+    settings: 'Instellingen',
+  },
+
+  generalInfo: {
+    profile_picture: 'Profielfoto',
+
+    personal_info: 'Persoonsgegevens',
+    first_name: 'Voornaam',
+    last_name: 'Achternaam',
+    phone: 'Telefoon',
+    email: 'Email',
+    address: 'Adres',
+    postcode: 'Postcode',
+
+    social_media: 'Sociale media',
+    facebook: 'Facebook profiel',
+    instagram: 'Instagram profiel',
+    other: '',
+
+    proof_address: 'Proof of address',
+    proof_address_description1:
+      'Before we can make your profile live we need proof of your address. We accept photo/scan of a physical letter or PDF copies of your digital statements within the past year.',
+    proof_address_description2: 'Read more on the documents we accept',
+  },
+
+  catSitterProfile: {
+    view_my_profile: 'Bekijk mijn openbare kattenoppasprofiel',
+
+    about_me: 'Over mij',
+    about_me_description:
+      'Tell cat owners about yourself. Start with a little description of yourself - What do you do for a living? Why do you want to be a cat sitter?',
+
+    experience_and_serivce: 'Ervaring en service',
+    experience_description:
+      'Please select relevant experience / skills you possess. For every item selected, please briefly explain the details in the text box below.',
+    has_cat: 'Eigenaar van een kat',
+    volunteer: 'Heeft vrijwillegerswerk gedaan',
+    administer_medication: 'Kan medicijnen toedienen ',
+    do_injections: 'Kan injecties toedienen',
+    certificate: 'Heeft certificaat voor dierenverzorging ',
+    grooming_skills: 'Heeft verzorgings bekwaamheid voor huisdieren',
+
+    pricing: 'Pricing',
+    one_day: 'One day visit',
+    per_hour: 'per hour',
+    overnight: 'Overnight visit',
+    per_night: 'per night',
+
+    availability: 'Beschikbaarheid',
+    availability_description:
+      'Select the dates that you are not available, so that cat owners can send you requests based on your availability.',
+    available: 'Beschikbaar',
+    unavailable: 'Niet beschikbaar',
+
+    emergency_contact: 'Emergency contact',
+    emergency_description: 'In case of an emergency, cat owners can contact the following:',
+    contact_full_name: 'Contact full name',
+    contact_number: 'Contact number',
+  },
+
+  catOwnerProfile: {
+    view_profile: 'Bekijk mijn openbare katteneigenaarsprofiel',
+
+    about_me: 'Over mij',
+    about_me_description: '',
+
+    appointment: 'Cat sitting appointment',
+    one_day: 'One day visit',
+    date: 'Date',
+    start_time: 'Start time',
+    end_time: 'End time',
+    overnight: 'Overnight visit',
+    start_date: 'Start date',
+    end_date: 'End date',
+    add_period: 'Add another period',
+    remove: 'Remove',
+
+    about_cat: 'Over mijn kat',
+    name: 'Naam',
+    age: 'Leeftijd',
+    gender: 'Geslacht',
+    male: 'Man',
+    female: 'Vrouw',
+    medical_needs: 'Medical needs',
+    injection: 'Injecties',
+    pill: 'Pil',
+    vaccinated: 'Gevaccineerd',
+    insured: 'Verzekerd',
+    yes: 'Ja',
+    no: 'Nee',
+    breed: 'Ras',
+    favourite_treat: 'Favoriete traktatie',
+    pictures: "Foto's van je kat",
+    add_cat: 'Add another cat',
+    remove_cat: 'Remove cat',
+
+    cat_description: 'Description of my cat(s)',
+    cat_description_text: '',
+  },
+
+  bookings: {
+    sitting_jobs: 'Oppas jobs voor mij',
+    sitting_service: 'Oppas diensten voor mijn kat',
+
+    requested: 'Requested',
+    confirmed: 'Confirmed',
+    completed: 'Completed',
+    reviews: 'Reviews',
+
+    view_profile: 'Bekijk profiel',
+    view_conversation: 'Bekijk gesprek',
+
+    area: '',
+    sitter_needed: '',
+
+    schedule_meetup: 'Plan eerst een afspraak',
+    reject: 'Afwijzen',
+    accept: 'Accepteren',
   },
 };
