e1ae87011f7d1f76a39477ee3760df5b19f9cb1d
Cherie Cheung <cheriesofteng@gmail.com>
2021-05-08 11:31:59 +0800

    [UI] Add 'Remark' component

---
+++ client/src/components/UIComponents/Text/Remark.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { themeColor } from '../../../style/theme'
+
+const Remark = styled.p`
+  font-size: 0.9rem;
+  color: ${themeColor.grey};
+`
+
+export default Remark
